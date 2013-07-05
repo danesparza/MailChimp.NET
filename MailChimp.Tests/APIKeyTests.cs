49c5311c143d5db1c22d867911802ea518a1b613
Dan Esparza <esparza.dan@gmail.com>
2013-07-05 13:38:13 -0400

    Initial check-in.  Wrapper assembly and tests

---
+++ MailChimp.Tests/APIKeyTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MailChimp.Tests
+{
+    [TestClass]
+    public class APIKeyTests
+    {
+        [TestMethod]
+        public void ValidAPIKey_ReturnsCorrectDataCenter()
+        {
+            //  Arrange
+            string apiKey = "testapikey-us2";
+
+            //  Act
+            MailChimp mc = new MailChimp(apiKey);
+
+            PrivateObject po = new PrivateObject(mc);
+            string dataCenterPrefix = po.GetField("_dataCenterPrefix").ToString();
+
+            //  Assert
+            Assert.AreEqual<string>("us2", dataCenterPrefix);
+
+        }
+    }
+}
---
+++ MailChimp/MailChimp.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailChimp
+{
+    /// <summary>
+    /// .NET API Wrapper for the Mailchimp v2.0 API.  
+    /// More information here: http://apidocs.mailchimp.com/api/2.0/
+    /// </summary>
+    public class MailChimp
+    {
+        /// <summary>
+        /// The HTTPS endpoint for the API.  
+        /// See http://apidocs.mailchimp.com/api/2.0/#api-endpoints for more information
+        /// </summary>
+        private string _httpsUrl = "https://{0}.api.mailchimp.com/2.0/{1}.json";
+
+        /// <summary>
+        /// The datacenter prefix.  This will be automatically determined
+        /// based on your API key
+        /// </summary>
+        private string _dataCenterPrefix = string.Empty;
+
+        //  Default constructor
+        public MailChimp()
+        {
+
+        }
+
+        /// <summary>
+        /// Get the datacenter prefix based on the API key passed
+        /// </summary>
+        /// <param name="apiKey">v2.0 Mailchimp API key</param>
+        /// <returns></returns>
+        private string GetDatacenterPrefix(string apiKey)
+        {
+            //  The key should contain a '-'.  If it doesn't, throw an exception:
+            if(!apiKey.Contains('-'))
+            {
+                throw new ArgumentException("API key is not valid.  Must be a valid v2.0 Mailchimp API key.");
+            }
+
+            return apiKey.Split('-')[1];
+        }
+
+        /// <summary>
+        /// Create an instance of the wrappper with your API key
+        /// </summary>
+        /// <param name="apiKey">The MailChimp API key to use</param>
+        public MailChimp(string apiKey) : this()
+        {
+            this.APIKey = apiKey;
+            this._dataCenterPrefix = GetDatacenterPrefix(apiKey);
+        }
+
+        /// <summary>
+        /// Gets / sets your API key for all operations.  
+        /// For help getting your API key, please see 
+        /// http://kb.mailchimp.com/article/where-can-i-find-my-api-key
+        /// </summary>
+        public string APIKey
+        {
+            get;
+            set;
+        }
+    }
+}
