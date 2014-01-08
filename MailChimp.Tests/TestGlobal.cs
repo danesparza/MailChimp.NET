dda53410ea3309d9282978eab73c0abc24840eb2
Dan Esparza <esparza.dan@gmail.com>
2014-01-08 10:52:57 -0500

    Updated test API key
    Old test API key has been removed from Mailchimp account

--- MailChimp.Tests/TestGlobal.cs
+++ MailChimp.Tests/TestGlobal.cs
@@ -20,7 +20,7 @@ namespace MailChimp.Tests
             //  Set this to your Mailchimp API key for testing
             //  See http://kb.mailchimp.com/article/where-can-i-find-my-api-key
             //  for help finding your API key
-            Test_APIKey = "d57fd22b29ec145774847c7709ca5d0f-us2";
+            Test_APIKey = "yourapikeyhere-us2";
         }
     }
 }
