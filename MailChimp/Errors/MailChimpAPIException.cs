a819b2ec99025b751984bbe67c31fd9a62ee3ba5
AndrewKoransky <andrew@tandasoft.com>
2014-02-06 16:07:04 -0700

    Exception handling adjustment
    IMHO, the exception should present the true nature of the problem at the outermost layer if possible.  I was getting "500 Internal Server Error" as the primary message which wasn't helpful.  I mean it may help debug the API, but it won't help the end user debug their code.

--- MailChimp/Errors/MailChimpAPIException.cs
+++ MailChimp/Errors/MailChimpAPIException.cs
@@ -8,9 +8,10 @@ namespace MailChimp.Errors
     /// </summary>
     public class MailChimpAPIException : Exception
     {
-        public MailChimpAPIException(string message, Exception innerException, ApiError apiError) 
-            : base(message, innerException)
+        public MailChimpAPIException(string message, Exception internalException, ApiError apiError)
+            : base(message)
         {
+            this.InternalException = internalException;
             this.MailChimpAPIError = apiError;
         }
 
@@ -18,6 +19,15 @@ namespace MailChimp.Errors
         /// The Mailchimp API specific error information
         /// </summary>
         public ApiError MailChimpAPIError
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// The internal exception that fired from within the MailChimp.NET error handling code
+        /// </summary>
+        public Exception InternalException
         {
             get;
             set;
