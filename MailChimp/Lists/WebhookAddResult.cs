14deda36b1b4612db9712e849d51c4fb8d2d1edc
Yagg <yagggg@gmail.com>
2014-05-15 15:43:38 +0400

    Implemented webhooks methods(see http://apidocs.mailchimp.com/webhooks/ for details)

---
+++ MailChimp/Lists/WebhookAddResult.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.Serialization;
+
+namespace MailChimp.Lists
+{
+    [DataContract]
+    public class WebhookAddResult
+    {
+        /// <summary>
+        /// the id of the new webhook
+        /// </summary>
+        [DataMember(Name = "id")]
+        public int WebhookId
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ MailChimp/Lists/WebhookInfo.cs
@@ -0,0 +1,42 @@
+﻿using System.Runtime.Serialization;
+
+namespace MailChimp.Lists
+{
+    /// <summary>
+    /// result of the operation including valid data and any errors
+    /// </summary>
+    [DataContract]
+    public class WebhookInfo
+    {
+        /// <summary>
+        /// the URL for this Webhook
+        /// </summary>
+        [DataMember(Name = "total")]
+        public string Url
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// the possible actions and whether they are enabled
+        /// </summary>
+        [DataMember(Name = "actions")]
+        public WebhookActions Actions
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// the possible sources and whether they are enabled
+        /// </summary>
+        [DataMember(Name = "sources")]
+        public WebhookSources Sources
+        {
+            get;
+            set;
+        }
+    }
+
+}
