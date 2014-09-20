63fe9b8eb71711a99c663d43af8185cd1e90a2c7
ZippyV <ZippyV@users.noreply.github.com>
2014-09-20 22:06:54 +0200

    Added all gallery methods

---
+++ MailChimp/Gallery/AddGalleryFolderResult.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.Serialization;
+
+namespace MailChimp.Gallery
+{
+    [DataContract]
+    public class AddGalleryFolderResult
+    {
+        /// <summary>
+        /// the id of the new folder
+        /// </summary>
+        [DataMember(Name = "id")]
+        public int Id
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// the name of the new folder
+        /// </summary>
+        [DataMember(Name = "name")]
+        public string Name
+        {
+            get;
+            set;
+        }
+    }
+}
