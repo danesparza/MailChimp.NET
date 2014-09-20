656ed1c45feff82b0316f9d744746b1bdecaadfb
ZippyV <ZippyV@users.noreply.github.com>
2014-09-20 22:06:54 +0200

    Added all gallery methods

---
+++ MailChimp/Gallery/GalleryListResult.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace MailChimp.Gallery
+{
+    [DataContract]
+    public class GalleryListResult
+    {
+        /// <summary>
+        /// the total matching items 
+        /// </summary>
+        [DataMember(Name = "total")]
+        public int Total
+        {
+            get;
+            set;
+        }
+
+        [DataMember(Name = "data")]
+        public List<Gallery> Galleries
+        {
+            get;
+            set;
+        }
+    }
+}
